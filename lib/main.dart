51a3530d52977e0840d4593a9adac6c709fd0816
Scalro <rotichcaleb200@gmail.com>
2022-11-24 14:18:52 +0300

    initial comit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(const MaterialApp());
+
+class MaterialApp extends StatefulWidget {
+  const MaterialApp({super.key});
+
+  @override
+  State<MaterialApp> createState() => _MaterialAppState();
+}
+
+class _MaterialAppState extends State<MaterialApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
